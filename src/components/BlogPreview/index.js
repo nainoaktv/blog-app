1aaf17a55bb71c3737d3019845ecd295d5c04d1f
nainoktv <nainoaktvillegas@gmail.com>
2023-12-26 21:18:18 -0800

    chore: dummy posts and template

--- src/components/BlogPreview/index.js
+++ src/components/BlogPreview/index.js
@@ -1,134 +1,119 @@
 import Link from "next/link";
 
-export default function BlogPreview() {
-  const date = "Dec 26th, 2023";
-  const category = "Tech";
-  const title = "Temporary Title";
-  const imageUrl =
-    "https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80";
-  const excerpt =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-  const authorImage =
-    "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250";
-  const authorName = "John Doe";
-  const authorRole = "Front-end Developer";
+const posts = [
+  {
+    id: 1,
+    title: "Boost your conversion rate",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    category: { title: "Marketing", href: "#" },
+    author: {
+      name: "Michael Foster",
+      role: "Co-Founder / CTO",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 2,
+    title: "Google layoffs, what to do next?",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "Nov 12, 2020",
+    datetime: "2020-03-16",
+    category: { title: "Business", href: "#" },
+    author: {
+      name: "Jane Doe",
+      role: "Front-end Developer",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 3,
+    title: "Jr Devs Harsh Reality",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "Dec 27, 2020",
+    datetime: "2020-03-16",
+    category: { title: "Technology", href: "#" },
+    author: {
+      name: "Smith Shea",
+      role: "Backend Developer",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+];
 
+export default function BlogPreview() {
   return (
-    <div className="w-full grid grid-cols-3 gap-4 mt-16">
-      <article className="flex flex-col md:space-x-4 mb-8">
-        <div className="relative w-full">
-          <img
-            src={imageUrl}
-            alt=""
-            className="w-full h-auto object-cover rounded"
-          />
-          <div className="absolute inset-0 bg-black opacity-40"></div>
+    <div className="bg-white py-24 sm:py-32 md:mt-16">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            From the blog
+          </h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">
+            Learn how to grow your business with our expert advice.
+          </p>
         </div>
-        <div className="w-full md:w-2/3">
-          <div className="flex items-center justify-between">
-            <time className="text-sm text-white">{date}</time>
-            <Link
-              className="text-sm text-amber-600 hover:text-amber-500"
-              href={`/#`}
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {posts.map((post) => (
+            <article
+              key={post.id}
+              className="flex max-w-xl flex-col items-start justify-between"
             >
-              {category}
-            </Link>
-          </div>
-          <h3 className="text-xl mt-2">
-            <Link className="text-amber-600 hover:text-amber-500" href="/#">
-              {title}
-            </Link>
-          </h3>
-          <p className="mt-2 text-white">{excerpt}</p>
-          <div className="flex items-center mt-4">
-            <img
-              src={authorImage}
-              alt={authorName}
-              className="w-8 h-8 rounded-full object-cover"
-            />
-            <div className="ml-2">
-              <p className="text-sm font-bold">{authorName}</p>
-              <p className="text-xs text-gray-500">{authorRole}</p>
-            </div>
-          </div>
+              <div className="flex items-center gap-x-4 text-xs">
+                <time dateTime={post.datetime} className="text-gray-500">
+                  {post.date}
+                </time>
+                <a
+                  href={post.category.href}
+                  className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                >
+                  {post.category.title}
+                </a>
+              </div>
+              <div className="group relative">
+                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                  <a href={post.href}>
+                    <span className="absolute inset-0" />
+                    {post.title}
+                  </a>
+                </h3>
+                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                  {post.description}
+                </p>
+              </div>
+              <div className="relative mt-8 flex items-center gap-x-4">
+                <img
+                  src={post.author.imageUrl}
+                  alt=""
+                  className="h-10 w-10 rounded-full bg-gray-50"
+                />
+                <div className="text-sm leading-6">
+                  <p className="font-semibold text-gray-900">
+                    <a href={post.author.href}>
+                      <span className="absolute inset-0" />
+                      {post.author.name}
+                    </a>
+                  </p>
+                  <p className="text-gray-600">{post.author.role}</p>
+                </div>
+              </div>
+            </article>
+          ))}
         </div>
-      </article>
-      <article className="flex flex-col md:space-x-4 mb-8">
-        <div className="relative w-full">
-          <img
-            src={imageUrl}
-            alt=""
-            className="w-full h-auto object-cover rounded"
-          />
-          <div className="absolute inset-0 bg-black opacity-40"></div>
-        </div>
-        <div className="w-full md:w-2/3">
-          <div className="flex items-center justify-between">
-            <time className="text-sm text-white">{date}</time>
-            <Link
-              className="text-sm text-amber-600 hover:text-amber-500"
-              href={`/#`}
-            >
-              {category}
-            </Link>
-          </div>
-          <h3 className="text-xl mt-2">
-            <Link className="text-amber-600 hover:text-amber-500" href="/#">
-              {title}
-            </Link>
-          </h3>
-          <p className="mt-2 text-white">{excerpt}</p>
-          <div className="flex items-center mt-4">
-            <img
-              src={authorImage}
-              alt={authorName}
-              className="w-8 h-8 rounded-full object-cover"
-            />
-            <div className="ml-2">
-              <p className="text-sm font-bold">{authorName}</p>
-              <p className="text-xs text-gray-500">{authorRole}</p>
-            </div>
-          </div>
-        </div>
-      </article>
-      <article className="flex flex-col md:space-x-4 mb-8">
-        <div className="relative w-full">
-          <img
-            src={imageUrl}
-            alt=""
-            className="w-full h-auto object-cover rounded"
-          />
-          <div className="absolute inset-0 bg-black opacity-40"></div>
-        </div>
-        <div className="w-full md:w-2/3">
-          <div className="flex items-center justify-between">
-            <time className="text-sm text-white">{date}</time>
-            <Link
-              className="text-sm text-amber-600 hover:text-amber-500"
-              href={`/#`}
-            >
-              {category}
-            </Link>
-          </div>
-          <h3 className="text-xl mt-2">
-            <Link className="text-amber-600 hover:text-amber-500" href="/#">
-              {title}
-            </Link>
-          </h3>
-          <p className="mt-2 text-white">{excerpt}</p>
-          <div className="flex items-center mt-4">
-            <img
-              src={authorImage}
-              alt={authorName}
-              className="w-8 h-8 rounded-full object-cover"
-            />
-            <div className="ml-2">
-              <p className="text-sm font-bold">{authorName}</p>
-              <p className="text-xs text-gray-500">{authorRole}</p>
-            </div>
-          </div>
-        </div>
-      </article>
+      </div>
     </div>
   );
 }
