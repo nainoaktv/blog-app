fb9acd5a6af9c81af4e8c6b1cd6cc88e5fafe162
nainoktv <nainoaktvillegas@gmail.com>
2023-12-26 19:49:29 -0800

    style: improvements and small device styling

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,11 +5,11 @@ import { useRouter } from "next/navigation";
 
 function NavItems({ router }) {
   return (
-    <div className="items-center justify-between w-full md:flex md:w-auto">
-      <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium font-sans md:flex-row md:space-x-8 md:mt-0 md:border-0">
+    <div className="items-center w-auto justify-between md:flex md:w-auto">
+      <ul className="flex flex-col  p-4 md:p-0 mt-4 font-medium font-mono md:flex-row md:space-x-8 md:mt-0 md:border-0">
         {navOptions.map((item) => (
           <li
-            className="cursor-pointer block py-2 pl-3 pr-4 text-amber-500 md:p-0"
+            className="cursor-pointer block py-2 pl-4 pr-4 mb-2 bg-amber-600 hover:bg-amber-500 text-black rounded md:p-1 transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 shadow-lg shadow-amber-400/30"
             key={item.id}
             onClick={() => router.push(item.path)}
           >
@@ -26,8 +26,10 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="fixed w-full top-0 left-0 z-20 border-b border-amber-500 bg-green-950">
-        <NavItems router={router} />
+      <nav className="fixed w-full top-0 left-0 z-20 border-b  bg-green-950">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <NavItems router={router} />
+        </div>
       </nav>
     </>
   );
