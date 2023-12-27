24aa7da1c576843ab8dd924f774d4c7f6c1609a8
nainoktv <nainoaktvillegas@gmail.com>
2023-12-26 19:02:50 -0800

    chore: add utils for nav path and labels

---
+++ src/utils/index.js
@@ -0,0 +1,22 @@
+export const navOptions = [
+  {
+    id: "home",
+    label: "Home",
+    path: "/",
+  },
+  {
+    id: "blog",
+    label: "Blog",
+    path: "/blog",
+  },
+  {
+    id: "about",
+    label: "About",
+    path: "/about",
+  },
+  {
+    id: "contact",
+    label: "Contact",
+    path: "/contact",
+  },
+];
