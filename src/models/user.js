d5cc1e3a3d414af179bfa276856afc7de29bed8b
nainoktv <nainoaktvillegas@gmail.com>
2023-12-27 21:46:25 -0800

    chore: add name to user

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
   username: {
     type: String,
     required: true,
