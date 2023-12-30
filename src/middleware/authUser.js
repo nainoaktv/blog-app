c01ce1ebe29eaecbd6adba23ead93e4d542b616c
nainoktv <nainoaktvillegas@gmail.com>
2023-12-29 21:37:32 -0800

    chore: auth user logic

---
+++ src/middleware/authUser.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+export const dynamic = "force-dynamic";
+
+const authUser = async (req) => {
+  const token = req.headers.get("Authorization")?.split(" ")[1];
+
+  if (!token) return false;
+
+  console.log(token);
+
+  try {
+    const extractAuthUserInfo = jwt.verify(token, "default_secret_key");
+
+    if (extractAuthUserInfo) return extractAuthUserInfo;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+export default authUser;
