6873fa5818362029acb80b8b1054864534360bf2
nainoktv <nainoaktvillegas@gmail.com>
2023-12-29 21:35:46 -0800

    feat: register form initial commit

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,4 +1,7 @@
+"use client";
 import { useState } from "react";
+
+// TODO: Include confirm password and improve form functionality.
 
 const initialFormData = {
   name: "",
@@ -10,4 +13,150 @@ const initialFormData = {
 
 export default function Register() {
   const [formData, setFormData] = useState(initialFormData);
+
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          {/* <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          /> */}
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+            Register Here
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6">
+            {/* EMAIL INPUT */}
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-yellow-800 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            {/* NAME INPUT */}
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-yellow-800 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            {/* USERNAME INPUT */}
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-yellow-800 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            {/* ROLE INPUT */}
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Role
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-yellow-800 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            {/* PASSWORD INPUT */}
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-white"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-amber-700 hover:text-amber-600"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-emerald-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{" "}
+            <a
+              href="#"
+              className="font-semibold leading-6 text-amber-700 hover:text-amber-600"
+            >
+              Continue as guest
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
